9d8fc8030a632fdec6a4cd26825b4c1ef3ca4055
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-02-11 12:59:48 +0000

    日付ボックスの表示を修正 インターフェースの変更ありエラーの状態

--- __tests__/components/calendar/DateBox.spec.tsx
+++ __tests__/components/calendar/DateBox.spec.tsx
@@ -3,9 +3,17 @@ import { render } from '@testing-library/react';
 import { DateBox } from '@/components/calendar/DateBox';
 
 describe('DateBox', () => {
-  it('displays the date passed through props', () => {
-    const { getByText } = render(<DateBox dateNumber={1} />);
-    const dateText = getByText('1');
+  it('日付が見える', () => {
+    const date = new Date("2023-05-21");
+    const { getByText } = render(<DateBox date={date} />);
+    const dateText = getByText('21');
     expect(dateText).toBeInTheDocument();
   });
+
+  // it('曜日が見える', () => {
+  //   const date = new Date("2023-05-21");
+  //   const { getByText } = render(<DateBox date={date} />);
+  //   const dayOfWeek = getByText('日');
+  //   expect(dayOfWeek).toBeInTheDocument();
+  // });
 });
