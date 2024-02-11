55089288884a925c8a5b41c91cf2b6930440216d
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-02-11 14:18:11 +0000

    カレンダーの日付ボックスの表示を修正

--- __tests__/components/calendar/DateBoxList.spec.tsx
+++ __tests__/components/calendar/DateBoxList.spec.tsx
@@ -5,14 +5,19 @@ import { DateBoxList } from '@/components/calendar/DateBoxList';
 describe('DateBoxList', () => {
   it('should render the correct number of DateBoxes', () => {
     const date = new Date("2024-02-01");
-    const { getAllByTestId, getByText } = render(<DateBoxList date={date} />);
-
-    const dateBoxes = getAllByTestId('date-box');
-    expect(dateBoxes).toHaveLength(29);
+    const { getByText } = render(<DateBoxList date={date} />);
 
     const firstDate = getByText('1');
     const lastDate = getByText('29');
     expect(firstDate).toBeInTheDocument();
     expect(lastDate).toBeInTheDocument();
+  });
+
+  it('should render blank DateBoxes before the first day of the month', () => {
+    const date = new Date("2024-02-01");
+    const { getAllByTestId } = render(<DateBoxList date={date} />);
+
+    const dateBoxes = getAllByTestId('date-box');
+    expect(dateBoxes).toHaveLength(33);
   });
 });
--- components/calendar/DateBoxList.tsx
+++ components/calendar/DateBoxList.tsx
@@ -4,14 +4,17 @@ export function DateBoxList({date}: {date: Date}) {
     const firstDate = new Date(date.getFullYear(), date.getMonth(), 1);
     const lastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
     const dateList = [];
+    for (let i = 1; i <= firstDate.getDay(); i++) {
+        dateList.push(undefined);
+    }
     for (let i = firstDate.getDate(); i <= lastDate.getDate(); i++) {
         dateList.push(new Date(date.getFullYear(), date.getMonth(), i));
     }
     return (
         <ul className="grid grid-cols-7 w-full">
             {dateList.map((date, index) => (
-                <li key={index}>
-                    <DateBox date={date} />
+                <li key={index} className="border border-gray-300">
+                    {date ? <DateBox date={date} /> : <DateBox blank />}
                 </li>
             ))}
         </ul>
