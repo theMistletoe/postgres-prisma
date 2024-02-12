36428ffbafd147ca24c427c8e6868d67d03f74ca
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-02-12 06:27:04 +0000

    DateBox.tsxの変更: onClickの引数をdateに変更***
    (Translation: ***Changes in DateBox.tsx: Changed onClick argument to date

--- components/calendar/DateBox.tsx
+++ components/calendar/DateBox.tsx
@@ -8,10 +8,17 @@ export function DateBox({
 {
     date?: Date,
     blank?: boolean
-    onClick: (event: React.MouseEvent<HTMLDivElement>) => void
+    onClick: (date: Date) => void
 }) {
+
+    function handleDateBoxClick(event: React.MouseEvent<HTMLDivElement>) {
+        event.preventDefault();
+        if (!date) return;
+        onClick(date);
+    }
+
     return (
-        <div data-testid="date-box" className="bg-white border border-gray-300 h-32" onClick={onClick}>
+        <div data-testid="date-box" className="bg-white border border-gray-300 h-32" onClick={handleDateBoxClick}>
             {!blank && date && (
                 <div className="flex m-2 justify-between">
                     <p>{date.getDate()}</p>
