c1cb92a1720c6d38cf02da85cf1f90f78e782c78
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-02-12 05:45:42 +0000

    debug用に追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { CalendarHeader } from '@/components/calendar/CalendarHeader'
 import { DateBoxList } from '@/components/calendar/DateBoxList'
 
 export const dynamic = 'force-dynamic'
@@ -5,6 +6,7 @@ export const dynamic = 'force-dynamic'
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
+      <CalendarHeader date={new Date()} />
       <DateBoxList date={new Date()} />
     </main>
   )
