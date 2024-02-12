8eb7fdb0ae334fd67e56c3556f754590c18b437e
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-02-12 06:13:38 +0000

    カレンダーのDateBoxコンポーネントにクリックイベントを追加

--- __tests__/components/calendar/DateBox.spec.tsx
+++ __tests__/components/calendar/DateBox.spec.tsx
@@ -1,24 +1,37 @@
 import { render } from '@testing-library/react';
 import { DateBox } from '@/components/calendar/DateBox';
+import userEvent from '@testing-library/user-event';
 
 describe('DateBox', () => {
+  const user = userEvent.setup();
+
   it('日付が見える', () => {
     const date = new Date("2023-05-21");
-    const { getByText } = render(<DateBox date={date} />);
+    const { getByText } = render(<DateBox date={date} onClick={vi.fn()} />);
     const dateText = getByText('21');
     expect(dateText).toBeInTheDocument();
   });
 
   it('曜日が見える', () => {
     const date = new Date("2023-05-21");
-    const { getByText } = render(<DateBox date={date} />);
+    const { getByText } = render(<DateBox date={date} onClick={vi.fn()} />);
     const dayOfWeek = getByText('日');
     expect(dayOfWeek).toBeInTheDocument();
   });
 
   it('blankオプションを渡すと、日付が見えない空欄セルが見える', async () => {
-    const { queryByText } = render(<DateBox blank />);
+    const { queryByText } = render(<DateBox blank onClick={vi.fn()} />);
     const cell = await queryByText("paragraph");
     expect(cell).not.toBeInTheDocument();
+  });
+
+  it("セルをクリックすると、渡された関数を実行する", async () => {
+    const date = new Date("2023-05-21");
+    const mockFn = vi.fn();
+    const { getByTestId } = render(<DateBox date={date} onClick={mockFn} />);
+    const cell = getByTestId("date-box");
+    await user.click(cell);
+
+    expect(mockFn).toHaveBeenCalled();
   });
 });
--- components/calendar/DateBox.tsx
+++ components/calendar/DateBox.tsx
@@ -3,13 +3,15 @@ import { translateDayOfWeek } from "@/lib/translateDayOfWeek";
 export function DateBox({
     date,
     blank,
+    onClick,
 }:
 {
     date?: Date,
     blank?: boolean
+    onClick: (event: React.MouseEvent<HTMLDivElement>) => void
 }) {
     return (
-        <div data-testid="date-box" className="bg-white border border-gray-300 h-32">
+        <div data-testid="date-box" className="bg-white border border-gray-300 h-32" onClick={onClick}>
             {!blank && date && (
                 <div className="flex m-2 justify-between">
                     <p>{date.getDate()}</p>
