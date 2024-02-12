44dd9e63b09c52db34afe41707d2a7bb428f87e4
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-02-12 05:34:33 +0000

    日付ボックスリストの日付を現在の日付に変更

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export const dynamic = 'force-dynamic'
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
-      <DateBoxList date={new Date("2024-07-06")} />
+      <DateBoxList date={new Date()} />
     </main>
   )
 }
