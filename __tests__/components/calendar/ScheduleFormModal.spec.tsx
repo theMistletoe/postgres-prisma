656c5aae765107dddf325d28a5dae015d3f7a068
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-02-21 12:33:04 +0000

    テストフォームモーダルの変更を追加

--- __tests__/components/calendar/ScheduleFormModal.spec.tsx
+++ __tests__/components/calendar/ScheduleFormModal.spec.tsx
@@ -1,13 +1,30 @@
 import { ScheduleFormModal } from '@/components/calendar/ScheduleFormModal';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
+
+const fetchMock = vi.fn();
+fetchMock.mockReturnValue(
+    Promise.resolve({
+        json: () => Promise.resolve({
+            id: 132,
+            title: "テスト予定",
+            startTime: new Date("2023-05-21T10:00:00.000Z"),
+            endTime: new Date("2023-05-21T11:00:00.000Z"),
+        }),
+    }),
+);
+vi.stubGlobal('fetch', fetchMock);
 
 describe('ScheduleFormModal', () => {
     const user = userEvent.setup();
 
+    afterEach(() => {
+        vi.clearAllMocks();
+    });
+
     it('タイトルが見える', async () => {
         const date = new Date("2023-05-21");
-        const { findByText } = render(<ScheduleFormModal date={date} isOpen={true} onClose={vi.fn} />); 
+        const { findByText } = render(<ScheduleFormModal date={date} isOpen={true} onClose={vi.fn} onScheduleCreate={vi.fn()} />); 
 
         const title = await findByText('2023-05-21の予定');
         expect(title).toBeInTheDocument();
@@ -15,7 +32,7 @@ describe('ScheduleFormModal', () => {
 
     it('モーダルを非表示にできる', async () => {
         const date = new Date("2023-05-21");
-        const { queryByText } = render(<ScheduleFormModal date={date} isOpen={false} onClose={vi.fn} />); 
+        const { queryByText } = render(<ScheduleFormModal date={date} isOpen={false} onClose={vi.fn} onScheduleCreate={vi.fn()} />); 
 
         const title = await queryByText('2023-05-21の予定');
         expect(title).not.toBeInTheDocument();
@@ -24,11 +41,39 @@ describe('ScheduleFormModal', () => {
     it('閉じるボタンを押すと、モーダルが閉じられる。', async () => {
         const date = new Date("2023-05-21");
         const handleClose = vi.fn();
-        const { findByText } = render(<ScheduleFormModal date={date} isOpen={true} onClose={handleClose} />); 
+        const { findByText } = render(<ScheduleFormModal date={date} isOpen={true} onClose={handleClose} onScheduleCreate={vi.fn()} />); 
 
         const closeButton = await findByText('閉じる');
         await user.click(closeButton);
 
         expect(handleClose).toHaveBeenCalled();
+    });
+
+    it('スケジュールを登録することができる。', async () => {
+        const date = new Date("2023-05-21");
+        const handleClose = vi.fn();
+        const handleScheduleCreate = vi.fn();
+        const { findByPlaceholderText, findByText, queryByText } = render(<ScheduleFormModal date={date} isOpen={true} onClose={handleClose} onScheduleCreate={handleScheduleCreate} />); 
+
+        const titleInput = await findByPlaceholderText('予定タイトル');
+        const startTimeInput = await findByPlaceholderText('開始時刻');
+        const endTimeInput = await findByPlaceholderText('終了時刻');
+
+        await user.type(titleInput, 'テスト予定');
+        await user.type(startTimeInput, '10:00');
+        await user.type(endTimeInput, '11:00');
+
+        const saveButton = await findByText('保存');
+        await user.click(saveButton);
+
+        await waitFor(() => {
+            expect(handleClose).toHaveBeenCalled();
+            expect(handleScheduleCreate).toHaveBeenCalledWith({
+                id: 132,
+                title: 'テスト予定',
+                startTime: new Date("2023-05-21T10:00:00"),
+                endTime: new Date("2023-05-21T11:00:00"),
+            });
+        });
     });
 });
