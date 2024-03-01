f35f2b2892de0382eb2a420f3c599a575d6bf53a
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-03-01 12:29:17 +0000

    useScheduleフックに対するテストを追加しました
    既存のPOSTの部分をリファクタしつつ、
    GETする場合のテストを追加

--- __tests__/hooks/useSchedule.spec.tsx
+++ __tests__/hooks/useSchedule.spec.tsx
@@ -1,8 +1,8 @@
 import { useSchedule } from "@/hooks/useSchedule";
 import { renderHook, waitFor } from "@testing-library/react";
 
-const fetchMock = vi.fn();
-fetchMock.mockReturnValue(
+const fetchPOSTMock = vi.fn();
+fetchPOSTMock.mockReturnValue(
     Promise.resolve({
         json: () => Promise.resolve({
             id: 132,
@@ -12,15 +12,41 @@ fetchMock.mockReturnValue(
         }),
     }),
 );
-vi.stubGlobal('fetch', fetchMock);
+
+const fetchGETMock = vi.fn();
+fetchGETMock.mockReturnValue(
+    Promise.resolve({
+        json: () => Promise.resolve([
+            {
+                id: 132,
+                title: "テスト予定",
+                startTime: new Date("2023-05-21T10:00:00.000Z"),
+                endTime: new Date("2023-05-21T11:00:00.000Z"),
+            },
+            {
+                id: 133,
+                title: "テスト予定2",
+                startTime: new Date("2023-05-21T12:00:00.000Z"),
+                endTime: new Date("2023-05-21T13:00:00.000Z"),
+            },
+            {
+                id: 134,
+                title: "テスト予定3",
+                startTime: new Date("2023-05-21T14:00:00.000Z"),
+                endTime: new Date("2023-05-21T15:00:00.000Z"),
+            }
+        ]),
+    }),
+);
 
 describe('useSchedule', () => {
-
+    
     afterEach(() => {
         vi.clearAllMocks();
     });
-
+    
     it('スケジュールを追加できる', async () => {
+        vi.stubGlobal('fetch', fetchPOSTMock);
         const { result } = renderHook(() => useSchedule());
         const { addSchedule } = result.current;
 
@@ -31,5 +57,20 @@ describe('useSchedule', () => {
         });
 
         await waitFor(() => expect(result.current.schedules).toHaveLength(1));
+    });
+
+    it('スケジュール一覧を取得できる', async () => {
+        vi.stubGlobal('fetch', fetchGETMock);
+        const { result } = renderHook(() => useSchedule());
+        const { getSchedules } = result.current;
+
+        await getSchedules();
+
+        await waitFor(() => expect(result.current.schedules).toHaveLength(3));
+        await waitFor(() => {
+            expect(result.current.schedules[0].title).toBe('テスト予定');
+            expect(result.current.schedules[1].title).toBe('テスト予定2');
+            expect(result.current.schedules[2].title).toBe('テスト予定3');
+        })
     });
 });
