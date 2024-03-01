5b44bfee2a4ec8c22c3abbab081be934927a47b5
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-03-01 13:29:24 +0000

    カレンダーのDateBoxコンポーネントに当日の予定が表示されるように修正

--- __tests__/components/calendar/DateBox.spec.tsx
+++ __tests__/components/calendar/DateBox.spec.tsx
@@ -34,4 +34,34 @@ describe('DateBox', () => {
 
     expect(mockFn).toHaveBeenCalled();
   });
+
+  it('当日の予定が見える', async () => {
+    const date = new Date("2023-05-21");
+    const { getByText } = render(<DateBox date={date} schedules={[
+      {
+        id: 132,
+        title: "テスト予定",
+        startTime: new Date("2023-05-21T10:00:00.000Z"),
+        endTime: new Date("2023-05-21T11:00:00.000Z"),
+      },
+      {
+        id: 133,
+        title: "テスト予定2",
+        startTime: new Date("2023-05-21T12:00:00.000Z"),
+        endTime: new Date("2023-05-21T13:00:00.000Z"),
+      },
+      {
+        id: 134,
+        title: "テスト予定3",
+        startTime: new Date("2023-05-21T14:00:00.000Z"),
+        endTime: new Date("2023-05-21T15:00:00.000Z"),
+      }
+    ]} onClick={vi.fn()} />);
+    const schedule1 = getByText('テスト予定');
+    const schedule2 = getByText('テスト予定2');
+    const schedule3 = getByText('テスト予定3');
+    expect(schedule1).toBeInTheDocument();
+    expect(schedule2).toBeInTheDocument();
+    expect(schedule3).toBeInTheDocument();
+  });
 });
