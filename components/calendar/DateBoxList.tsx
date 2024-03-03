6e9d3663734f43af7d2364c0a9495d7e394f39cb
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-03-03 09:24:21 +0000

    DateBoxListコンポーネントの修正
    リファクタリングを実行

--- components/calendar/DateBoxList.tsx
+++ components/calendar/DateBoxList.tsx
@@ -1,34 +1,27 @@
 import { Schedule } from "@prisma/client";
 import { DateBox } from "./DateBox";
 
-export function DateBoxList(
-    {
-        date,
-        schedules,
-        onClick
-    }:
-    {
-        date: Date,
-        schedules?: Schedule[],
-        onClick: (event: Date) => void
-    }
-) {
+type DateBoxListProps = {
+    date: Date,
+    schedules?: Schedule[],
+    onClick: (event: Date) => void
+}
+
+export function DateBoxList({ date, schedules, onClick }: DateBoxListProps) {
     const firstDate = new Date(date.getFullYear(), date.getMonth(), 1);
     const lastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-    const dateList = [];
-    for (let i = 1; i <= firstDate.getDay(); i++) {
-        dateList.push(undefined);
-    }
-    for (let i = firstDate.getDate(); i <= lastDate.getDate(); i++) {
-        dateList.push(new Date(date.getFullYear(), date.getMonth(), i));
-    }
+    const dateList = Array(firstDate.getDay()).fill(undefined)
+        .concat(
+            Array.from({ length: lastDate.getDate() - firstDate.getDate() + 1 }, (_, i) => new Date(date.getFullYear(), date.getMonth(), i + firstDate.getDate()))
+        );
+
     return (
         <ul className="grid grid-cols-7 w-full">
             {dateList.map((date, index) => {
                 const filteredSchedulesByDay = schedules?.filter((schedule) => {
                     return date?.toISOString().slice(0, 10) === schedule.startTime.toISOString().slice(0, 10);
-                }); 
-                
+                });
+
                 return (
                     <li key={index} >
                         {date ? <DateBox date={date} onClick={onClick} schedules={filteredSchedulesByDay} /> : <DateBox blank onClick={onClick} />}
