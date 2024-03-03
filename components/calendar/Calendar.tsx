57484f2200a10df7eb4bdd712e9d5b86ca65c7bb
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-03-03 12:47:40 +0000

    カレンダーコンポーネントにスケジュールの取得と表示を追加
    実装する

--- components/calendar/Calendar.tsx
+++ components/calendar/Calendar.tsx
@@ -1,23 +1,29 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CalendarHeader } from "./CalendarHeader";
 import { DateBoxList } from "./DateBoxList";
 import { ScheduleFormModal } from "./ScheduleFormModal";
+import { useSchedule } from "@/hooks/useSchedule";
 
 export default function Calendar({ date }: { date: Date }) {
     const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined);
     const [isOpen, setIsOpen] = useState<boolean>(false)
+    const {schedules, getSchedules} = useSchedule();
 
     function handleDateBoxClick(date: Date) {
         setSelectedDate(date);
         setIsOpen(true);
     }
 
+    useEffect(() => {
+        getSchedules();
+    }, [date]);
+
     return (
         <>
             <CalendarHeader date={date} />
-            <DateBoxList date={date} onClick={handleDateBoxClick} />
+            <DateBoxList date={date} onClick={handleDateBoxClick} schedules={schedules} />
             {selectedDate && (
                 <ScheduleFormModal date={selectedDate} isOpen={isOpen} onClose={() => setIsOpen(false)} onScheduleCreate={() => {}} />
             )}
