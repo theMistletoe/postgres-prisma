15e6d6b0fe36a51a9707c36cf4a1eabd338a077a
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-03-03 09:00:13 +0000

    テスト予定のタイトルを変更しました
    data-testidの内容を修正するべくテスト修正

--- __tests__/components/calendar/DateBox.spec.tsx
+++ __tests__/components/calendar/DateBox.spec.tsx
@@ -5,7 +5,7 @@ import userEvent from '@testing-library/user-event';
 const schedules = [
   {
     id: 132,
-    title: "テスト予定",
+    title: "テスト予定1",
     startTime: new Date("2023-05-21T10:00:00.000Z"),
     endTime: new Date("2023-05-21T11:00:00.000Z"),
   },
@@ -50,7 +50,7 @@ describe('DateBox', () => {
     const date = new Date("2023-05-21");
     const mockFn = vi.fn();
     const { getByTestId } = render(<DateBox date={date} onClick={mockFn} />);
-    const cell = getByTestId("date-box");
+    const cell = getByTestId("date-box_2023-05-21");
     await user.click(cell);
 
     expect(mockFn).toHaveBeenCalled();
@@ -60,7 +60,7 @@ describe('DateBox', () => {
     const date = new Date("2023-05-21");
     const { getByText } = render(<DateBox date={date} schedules={schedules} onClick={vi.fn()} />);
 
-    expect(getByText('テスト予定')).toBeVisible();
+    expect(getByText('テスト予定1')).toBeVisible();
     expect(getByText('テスト予定2')).toBeVisible();
     expect(getByText('テスト予定3')).toBeVisible();
   });
