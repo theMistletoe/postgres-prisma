ecb733b66b0345f465e40fab6e3f582a7882bd7c
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-03-04 12:51:14 +0000

    日付の表示形式を修正

--- lib/formatDisplayDate.ts
+++ lib/formatDisplayDate.ts
@@ -1,3 +1,3 @@
 export function formatDisplayDate(date: Date){
-    return "not implemented yet";
+    return date.getFullYear() + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + ('0' + date.getDate()).slice(-2);
 }
